cdd3cb784c9c2d2db998b7ba88cee37ad9745449
Braeden Neale <spartan.nord@gmail.com>
2021-02-17 20:02:16 -0700

    cleaned up

--- test.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-NUM="2";
-FOLDER="1-100";
-TESTFILE="PassOff/"$FOLDER"/input"$NUM".txt";
-ORIGPRINT="PassOff/"$FOLDER"/answer"$NUM".txt";
-MYPRINT="passOffOut"$FOLDER"-"${NUM}".txt";
-
-./main $TESTFILE > $MYPRINT;
-diff $ORIGPRINT $MYPRINT;
